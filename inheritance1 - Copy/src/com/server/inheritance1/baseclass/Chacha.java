81bf2a935d082e26a3dd88848a4556fb04be452e
unknown <nageshkoppad.xworkz@gmail.com>
2022-08-21 22:18:49 +0530

    inheritance

---
+++ inheritance1 - Copy/src/com/server/inheritance1/baseclass/Chacha.java
@@ -0,0 +1,18 @@
+package com.server.inheritance1.baseclass;
+
+public class Chacha {
+	public String name="gagan";
+	public int age=65;
+	public String place="beng";
+	public Chacha()
+	{
+		
+	}
+public Chacha(String name,int age, String place)
+{
+	this.name=name;
+	this.age=age;
+	this.place=place;
+}
+
+}
