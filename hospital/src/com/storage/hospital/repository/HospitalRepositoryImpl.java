a07c90ea2d59a49b6bd3c806ee0faeb655c44cda
unknown <nageshkoppad.xworkz@gmail.com>
2022-10-14 11:31:27 +0530

    storage

---
+++ hospital/src/com/storage/hospital/repository/HospitalRepositoryImpl.java
@@ -0,0 +1,17 @@
+package com.storage.hospital.repository;
+
+import com.storage.hospital.dto.HospitalDTO;
+
+public class HospitalRepositoryImpl implements HospitalRepository {
+
+	public HospitalRepositoryImpl() {
+
+	}
+
+	@Override
+	public boolean save(HospitalDTO dto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
